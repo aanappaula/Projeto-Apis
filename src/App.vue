0e7df11841e109572c711aa265d3dfff7bb042b4
Ana Paula <aninha10.desouza@gmail.com>
2022-09-21 10:52:22 -0300

    ok

---
+++ src/App.vue
@@ -0,0 +1,78 @@
+<script>
+import axios from "axios";
+export default {
+  data() {
+    return {
+      cep: "",
+      endereco: {},
+    };
+  },
+  methods: {
+    async buscar() {
+      const url = `https://viacep.com.br/ws/${this.cep}/json/`;
+      const res = await axios.get(url);
+      this.endereco = res.data;
+    },
+  },
+};
+</script>
+
+<template>
+  <main>
+    <h1>Buscar Cep</h1>
+    <form @submit.prevent="buscar">
+      <label for="rua">CEP:</label>
+      <input type="text" v-model="cep" placeholder="Digite o CEP:" />
+      <button type="submit">Buscar:</button>
+    </form>
+    <form class="end">
+      <label for="rua">Rua:</label>
+      <input type="text" v-model="endereco.logradouro" />
+    </form>
+    <form class="end">
+      <label for="numero"
+        >Número:
+        <input type="text" v-model="endereco.numero" />
+      </label>
+    </form>
+    <form class="end">
+      <label for="Bairro">Bairro:</label>
+      <input type="text" v-model="endereco.bairro" />
+    </form>
+    {{ endereco }}
+  </main>
+</template>
+
+<style>
+@import "@/assets/base.css";
+@import "@/assets/main.css";
+#app {
+  max-width: 5000px;
+  max-height: 1000px;
+  margin: 0 auto;
+  height: 100vh;
+  font-weight: normal;
+}
+header,
+footer {
+  background-color: rgb(126, 73, 126);
+  color: white;
+  height: 15%;
+  font-size: 1.3rem;
+  display: flex;
+  align-items: center;
+  padding-left: 2rem;
+}
+main {
+  background-color: pink;
+  padding: 40px;
+  border-radius: 20px;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+}
+input {
+  align-items: center;
+}
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,62 @@
+@import "./main.css";
+<template>
+  <main>
+    <form>
+      <div class="principal mb-3">
+        <label for="exampleInputEmail1" class="form-label"
+          >Insira o seu CEP:</label
+        >
+        <input
+          type="email"
+          class="form-control"
+          id="exampleInputEmail1"
+          aria-describedby="emailHelp"
+        />
+        <div id="emailHelp" class="form-text">
+          Seu cep não será compartilhado com ninguém!
+        </div>
+      </div>
+      <div class="mb-3 form-check">
+        <input type="checkbox" class="form-check-input" id="exampleCheck1" />
+        <label class="form-check-label" for="exampleCheck1">Concordo</label>
+      </div>
+      <button type="submit" class="btn btn-primary">Enviar</button>
+    </form>
+  </main>
+  <div class="principal mb-3">
+    <label for="exampleInputEmail1" class="form-label">logradouro</label>
+    <input
+      type="email"
+      class="form-control"
+      id="exampleInputEmail1"
+      aria-describedby="emailHelp"
+    />
+  </div>
+  <div class="principal mb-3">
+    <label for="exampleInputEmail1" class="form-label">Bairro</label>
+    <input
+      type="email"
+      class="form-control"
+      id="exampleInputEmail1"
+      aria-describedby="emailHelp"
+    />
+  </div>
+  <div class="principal mb-3">
+    <label for="exampleInputEmail1" class="form-label">Cidade</label>
+    <input
+      type="email"
+      class="form-control"
+      id="exampleInputEmail1"
+      aria-describedby="emailHelp"
+    />
+  </div>
+  <div class="principal mb-3">
+    <label for="exampleInputEmail1" class="form-label">uf</label>
+    <input
+      type="email"
+      class="form-control"
+      id="exampleInputEmail1"
+      aria-describedby="emailHelp"
+    />
+  </div>
+</template>
